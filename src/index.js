fb3e00a0982de0c988e3e6f3d5522a3bb3e5b803
Yeferson Buchinski <yefersonb@gmail.com>
2025-06-28 17:02:56 -0300

    versión inicial, build funcionando

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { UserProvider } from "./contexts/UserContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
   <UserProvider>
     <App />
