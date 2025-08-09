51b93e9c9c03201066852dd969b7c9dee4573875
Rhodexa <69089201+Rhodexa@users.noreply.github.com>
2025-08-09 17:52:13 -0300

    Add profile picture placeholder to header UI
    Introduced a profile picture placeholder image and updated the Header component to display it alongside a greeting. Refined header layout and styling in cozyglow.css, including adjustments to padding, height, and color variables, and added a shaded class for optional box-shadow.

--- MeVoy/src/components/Header.js
+++ MeVoy/src/components/Header.js
@@ -1,15 +1,20 @@
 import React from "react";
 import logo from "../assets/logo/logo_light.png"; // Optimization here is a pain!
+import pfp from "../assets/ui/profile_picture_placeholder.png"; // Optimization here is a pain!
 
 export default function Header() {
   return (
-    <header className="header">
-      <img src={logo} alt="MeVoy Logo" style={{ height: "100%" }} />
-      Viajes Compartidos
-      
-      {/* Profile picture should go here — placeholder*/}
-      <div style={{ height: "2rem", width: "2rem", backgroundColor: "gray", borderRadius: "50%" }}></div>
+    <header className="header shaded"> {/* The shaded class can be toggled to add flair to the UI (More on that later) */}
+      <div style={{ display: "flex", alignItems: "center", gap: "0.8rem", height: "90%" }}>
+        <img src={logo} alt="MeVoy Logo" style={{ height: "90%", opacity: "0.7" }} />
+        <div>Viajes Compartidos</div>
+      </div>
 
+      <div style={{ display: "flex", alignItems: "center", gap: "0.8rem", height: "90%" }}>
+        <div>Hola, <i>Nombre</i></div>
+        {/* Profile picture should go here — placeholder*/}
+        <img src={pfp} alt="Foto de Perfil" style={{ height: "100%", borderRadius: "50%", overflow: "hidden", opacity: "0.2" }} />
+      </div>
     </header>
   );
 }
